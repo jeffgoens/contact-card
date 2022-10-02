929efbf3b76eba76685ed9581a9a88d81e73ea6f
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-10-02 23:25:53 +0000

    Updated image

--- main.js
+++
@@ -1,9 +0,0 @@
-// let getProjectButton = document.getElementById("projectButton")
-
-// getProjectButton.addEventListener("click", () => {
-
-
-// })
-
-
-// getProjectButton.addEventListener("click", href="https://jeffgoens.github.io/mind-reader/");
